e3c8ef3c7677c585a07bf14c615eb134cc9337ba
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-28 17:04:29 +0100

    added make bread and brown bread production

--- factory_functions.py
+++ factory_functions.py
@@ -4,6 +4,21 @@ def make_dough(ingred_1, ingred_2):
     # when given water and flour, return dough
     if ingred_1 == "water" and ingred_2 == "flour":
         return "dough"
+    # what about wholegrain flour, should return brown dough
+    elif ingred_1 == "water" and ingred_2 == "wholegrain flour":
+        return "brown dough"
     # else, return not dough
     else:
         return 'not dough'
+
+
+def make_bread(ingred):
+    # when given dough, return bread
+    if ingred == "dough":
+        return "bread"
+    # what about the brown dough, should return brown bread
+    elif ingred == "brown dough":
+        return "brown bread"
+    # else return not bread
+    else:
+        return "not bread"
--- factory_functions_spec_tests.py
+++ factory_functions_spec_tests.py
@@ -7,6 +7,13 @@ class TestFactoryFunctions(unittest.TestCase):
     def test_make_dough(self):
         self.assertEqual(make_dough('water', 'flour'), 'dough')
         self.assertEqual(make_dough('water', 'cheese'), 'not dough')
+        self.assertEqual(make_dough('water', 'wholegrain flour'), 'brown dough')
+
+
+    def test_make_bread(self):
+        self.assertEqual(make_bread('dough'), 'bread')
+        self.assertEqual(make_bread('not dough'), 'not bread')
+        self.assertEqual(make_bread('brown dough'), 'brown bread')
 
 
 if __name__ == '__main__':
