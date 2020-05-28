8000ce99fb90881fd33bb4c802525c3f2b2edfbd
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-28 16:43:45 +0100

    resolved tests for make_dough

--- factory_functions.py
+++ factory_functions.py
@@ -2,5 +2,8 @@
 
 def make_dough(ingred_1, ingred_2):
     # when given water and flour, return dough
+    if ingred_1 == "water" and ingred_2 == "flour":
         return "dough"
     # else, return not dough
+    else:
+        return 'not dough'
